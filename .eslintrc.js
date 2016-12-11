e30aa3a458b82eadfede190e6da11f6a7724559c
sai.guntupalli <sai.guntupalli@lifelock.com>
2016-12-10 16:41:39 -0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: 'airbnb',
+  globals: {
+    __CLIENT__: true,
+    __DEVTOOLS__: true,
+  }
+};
---
+++ src/containers/Home.jsx
@@ -0,0 +1,7 @@
+/* eslint react/prefer-stateless-function: 0 */
+import React from 'react';
+export default class Home extends React.Component {
+  render() {
+    return <div>Home</div>;
+  }
+}
